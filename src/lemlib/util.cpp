05bd3ac1d6efee38fdbd34ae5ce2b71442f70e77
Raptorly1 <95979895+Raptorly1@users.noreply.github.com>
2024-11-29 08:00:44 -0800

    test format

--- src/lemlib/util.cpp
+++ src/lemlib/util.cpp
@@ -8,10 +8,10 @@
  * @copyright Copyright (c) 2023
  *
  */
-#include <math.h>
-#include <vector>
 #include "lemlib/pose.hpp"
 #include "lemlib/util.hpp"
+#include <math.h>
+#include <vector>
 
 /**
  * @brief Slew rate limiter
@@ -22,15 +22,19 @@
  * @return float - the limited value
  */
 float lemlib::slew(float target, float current, float maxChange) {
-    float change = target - current;
-    if (maxChange == 0) return target;
-    if (change > maxChange) change = maxChange;
-    else if (change < -maxChange) change = -maxChange;
-    return current + change;
+  float change = target - current;
+  if (maxChange == 0)
+    return target;
+  if (change > maxChange)
+    change = maxChange;
+  else if (change < -maxChange)
+    change = -maxChange;
+  return current + change;
 }
 
 /**
- * @brief Calculate the error between 2 angles. Useful when calculating the error between 2 headings
+ * @brief Calculate the error between 2 angles. Useful when calculating the
+ * error between 2 headings
  *
  * @param angle1
  * @param angle2
@@ -38,7 +42,7 @@ float lemlib::slew(float target, float current, float maxChange) {
  * @return float wrapped angle
  */
 float lemlib::angleError(float angle1, float angle2, bool radians) {
-    return std::remainder(angle1 - angle2, radians ? 2 * M_PI : 360);
+  return std::remainder(angle1 - angle2, radians ? 2 * M_PI : 360);
 }
 
 /**
@@ -48,9 +52,11 @@ float lemlib::angleError(float angle1, float angle2, bool radians) {
  * @return float
  */
 float lemlib::avg(std::vector<float> values) {
-    float sum = 0;
-    for (float value : values) { sum += value; }
-    return sum / values.size();
+  float sum = 0;
+  for (float value : values) {
+    sum += value;
+  }
+  return sum / values.size();
 }
 
 /**
@@ -62,30 +68,33 @@ float lemlib::avg(std::vector<float> values) {
  * @return float - the smoothed output
  */
 float lemlib::ema(float current, float previous, float smooth) {
-    return (current * smooth) + (previous * (1 - smooth));
+  return (current * smooth) + (previous * (1 - smooth));
 }
 
 /**
- * @brief Get the signed curvature of a circle that intersects the first pose and the second pose
+ * @brief Get the signed curvature of a circle that intersects the first pose
+ * and the second pose
  *
  * @note The circle will be tangent to the theta value of the first pose
- * @note The curvature is signed. Positive curvature means the circle is going clockwise, negative means
- * counter-clockwise
- * @note Theta has to be in radians and in standard form. That means 0 is right and increases counter-clockwise
+ * @note The curvature is signed. Positive curvature means the circle is going
+ * clockwise, negative means counter-clockwise
+ * @note Theta has to be in radians and in standard form. That means 0 is right
+ * and increases counter-clockwise
  *
  * @param pose the first pose
  * @param other the second pose
  * @return float curvature
  */
 float lemlib::getCurvature(Pose pose, Pose other) {
-    // calculate whether the pose is on the left or right side of the circle
-    float side = lemlib::sgn(std::sin(pose.theta) * (other.x - pose.x) - std::cos(pose.theta) * (other.y - pose.y));
-    // calculate center point and radius
-    float a = -std::tan(pose.theta);
-    float c = std::tan(pose.theta) * pose.x - pose.y;
-    float x = std::fabs(a * other.x + other.y + c) / std::sqrt((a * a) + 1);
-    float d = std::hypot(other.x - pose.x, other.y - pose.y);
+  // calculate whether the pose is on the left or right side of the circle
+  float side = lemlib::sgn(std::sin(pose.theta) * (other.x - pose.x) -
+                           std::cos(pose.theta) * (other.y - pose.y));
+  // calculate center point and radius
+  float a = -std::tan(pose.theta);
+  float c = std::tan(pose.theta) * pose.x - pose.y;
+  float x = std::fabs(a * other.x + other.y + c) / std::sqrt((a * a) + 1);
+  float d = std::hypot(other.x - pose.x, other.y - pose.y);
 
-    // return curvature
-    return side * ((2 * x) / (d * d));
+  // return curvature
+  return side * ((2 * x) / (d * d));
 }
